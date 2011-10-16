2aeec3caeb9e6dc38e86a66b406ead839a3cedba
Maxwell Q Shron <mshron@bellatrix.cs.uchicago.edu>
2011-10-16 09:00:42 -0500

    Part geocoded.

--- geocode/geocode_json.py
+++ geocode/geocode_json.py
@@ -1,15 +1,19 @@
 '''Takes the json file on arg[1], which hopefully has an 'address' column, geocodes it, writes out to arg[2]'''
 
 from geocodeAddresses import geocode
+import sys
 import json
 import time
 
 def main():
     d = json.load(open(sys.argv[1]))
     out = open(sys.argv[2],'w')
-    for row in d:
-      row.update(geocode(row))
+    extra = sys.argv[3]
+    for i,row in enumerate(d):
+      row.update(geocode(row['address'] + extra))
       time.sleep(.1)
+      if i == 0:
+          break
     json.dump(d, out)
     
 if __name__ == "__main__":
