d481f4f9c0c05e380fdcfa6fdcc4553cfbdf9be3
Thomas Levine <thomas.levine@gmail.com>
2011-10-16 02:17:47 -0400

    off by one still but mostly works

--- drs-sfd.gouv.sn:8083/parse.py
+++ drs-sfd.gouv.sn:8083/parse.py
@@ -1,30 +1,44 @@
 #!/usr/bin/env python
 from lxml.html import fromstring
 from sys import argv
-from os import lsdir
+from os import listdir
 
 def main():
   datadir=argv[1]
   for f in listdir(datadir):
-    print parse(f)
+    print parse(datadir+'/'+f)
 
 def parse(f):
   """Parse a previously downloaded html file"""
-  handle=open(f,'r').read()
-  raw=fromstring(handle)
+  print f
+  handle=open(f,'r')
+  raw=handle.read()
   handle.close()
 
-  cells=xml.xpath('//th or td')
+  xml=fromstring(raw)
+  for tag in xml.xpath('//tr[@class="space"]'):
+    xml.remove(tag)
+  for tag in xml.xpath('/table[@class="sous-tableau"]'):
+    xml.remove(tag)
+
+  cells=xml.xpath('//*[self::th or self::td]')
+
   #Check for even-ness
-  assert len(cells) % 2 == 0
+  if (len(cells) % 2) != 0:
+#    raise AlignmentError
+    pass
 
   d={}
-  while len(cells)>0:
+  #while len(cells)>0:
+  while len(cells)>1:
     key=cells.pop().text
     value=cells.pop().text
     d[key]=value
 
   return d
+
+class AlignmentError(Exception):
+  pass
 
 if __name__ == '__main__':
   main()
