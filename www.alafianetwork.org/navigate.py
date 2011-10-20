3c1e576e0e66219b68f65d6cb37c64797ff891f3
Thomas Levine <thomas.levine@gmail.com>
2011-10-20 02:01:13 -0400

    getting html

--- www.alafianetwork.org/navigate.py
+++ www.alafianetwork.org/navigate.py
@@ -11,20 +11,31 @@ BASEURL='http://www.alafianetwork.org/repimf.php?page='
 
 def main():
   for p in [0]: #range(0,lastpage()+1):
-    xml=getpage(p)
-    print xml.xpath('//p')
+    d={
+      "page":p
+    , "html":getpage(p,xml=False)
+    }
+    save(['p'],d,'html')
 
 def lastpage():
   xml=getpage(0)
   return int(xml.xpath('//center/a')[-2].text)
 
-def get(url):
-  return fromstring(urlopen(url).read())
+def get(url,xml=True):
+  html=urlopen(url).read()
+  if xml:
+    return fromstring(html)
+  else:
+    return html
 
-def get_file(url):
-  return fromstring(open(url.split('/')[-1]).read())
+def get_file(url,xml=True):
+  html=open(url.split('/')[-1]).read()
+  if xml:
+    return fromstring(html)
+  else:
+    return html
 
-def getpage(number):
-  return get_file(BASEURL+str(number))
+def getpage(number,xml=True):
+  return get_file(BASEURL+str(number),xml)
 
 main()
