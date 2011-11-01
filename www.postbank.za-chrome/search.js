6749392fd1ae0a7331d0c3957ebdc553de9a7435
Thomas Levine <thomas.levine@gmail.com>
2011-11-01 14:03:07 -0400

    yay

--- www.postbank.za-chrome/search.js
+++ www.postbank.za-chrome/search.js
@@ -21,8 +21,43 @@ chainsaw={
       $('#Centralcolum3_btnSubmit').click();
     }
   }
+, "parse":function(){
+    basepath='#Centralcolum3_dtgGroup td > '
+    cell_nodes=$(basepath+'span , '+basepath+'strong');
+    d=this.makerow(cell_nodes);
+    return d;
+  }
+, "makerow":function(cell_nodes){
+    cells=[]
+    cell_nodes.each(function(){
+      cells.push($(this).text());
+    });
+    d={};
+    cells.pop(); //Remove that junk '1' at the end
+    //Go backwards because pop comes from the end.
+    while (cells.length>2){
+      value=this.getcellvalue(cells);
+      key=this.getcellvalue(cells);
+      d[key]=value;
+    }
+    d['loc2']=this.getcellvalue(cells);
+    d['loc1']=this.getcellvalue(cells);
+    return d;
+  }
+, "getcellvalue":function(cells){
+    return this.compact(cells.pop(0));
+  }
+, "compact":function(str){
+/*
+    str=str.replace('\n',' ');
+    str=str.replace('  +','').replace('^ *','').replace(' *$','');
+*/
+    return str;
+  }
 }
 
 jQuery(function(){
+  d=chainsaw.parse();
+  chainsaw.push([d]);
   chainsaw.grab();
 });
