2812651b5dce30585703a52869201aee532a1c05
Thomas Levine <thomas.levine@gmail.com>
2011-10-31 20:55:46 -0400

    postbank

--- www.postbank.za-tom/hand.py
+++ www.postbank.za-tom/hand.py
@@ -1,14 +1,38 @@
 #!/usr/bin/env python
 from demjson import decode
+from requests import post
 
 def main():
+  pass
+  #print wit['request'].keys()
+
+def grab(identifier):
   wit=load_har('witspos.har')
-  print wit
+  print 'Loaded the HAR'
+  response=post(
+    url=wit['url']
+  , data=wit['params']
+  , headers=wit['headers']
+  )
+  print 'Sent the request'
+  print response.content
 
 def load_har(filename):
+  """Load a HAR file from which we'll determine the requests."""
   h=open(filename,'r')
   har=decode(h.read())
   h.close()
-  return har
+  out={
+    "url":har['request']['url']
+  , "headers":dictionarify(har['request']['headers'])
+  , "params":dictionarify(har['request']['postData']['params'])
+  }
+  return out
 
-main()
+def dictionarify(harlist):
+  out={}
+  for pair in harlist:
+    out[pair['name']]=pair['value']
+  return out
+
+grab(23)
